158daa9c2516e0f7bc1e738a64f463c39e119776
Adrian <an.stasiv@gmail.com>
2021-02-28 18:22:17 +0200

    sc

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,3 @@
 <template>
   <div>hello</div>
 </template>
-
-<script>
-export default {
-  head() {
-    return {
-      script: [
-        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
-      ],
-    }
-  },
-}
-</script>
