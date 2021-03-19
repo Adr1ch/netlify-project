57ae049accac5ddb2ca7e972251a62a810aaf374
Adrian <an.stasiv@gmail.com>
2021-03-19 17:34:28 +0200

    netlify config update

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>hello</div>
+  <div>
+    <h1>Сайт про рибалку</h1>
+    <h2>Як ловити карася?</h2>
+    <h2>Як ловити коропа?</h2>
+    <h2>Як ловити рибу?</h2>
+    <div>{{ data }}</div>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const data = await $content('careers').fetch()
+    return { data }
+  },
+}
+</script>
